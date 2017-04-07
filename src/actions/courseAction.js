a56366c8e00529f983860568f83220de9e468603
quy <leuducquy@gmail.com>
2017-04-07 11:56:56 +0700

    modify something

--- src/actions/courseAction.js
+++ src/actions/courseAction.js
@@ -1,8 +1,10 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
     console.log('action');
-    return {type : 'CREATE_COURSE',course};
+    return {type : types.CREATE_COURSE};
 }
-export function deleteCourse(course) {
-    console.log('action');
-    return {type : 'DELETE_COURSE',course};
-}
+// export function deleteCourse(course) {
+//     console.log('action');
+//     return {type : 'DELETE_COURSE',course};
+// }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,7 +1,10 @@
+import * as types from '../actions/actionTypes';
+
+
 export default function courseReducer(state = [] , action){
 
     switch (action.type) {
-        case 'CREATE_COURSE' :
+        case types.CREATE_COURSE :
       
         console.log('reducer');
          return [...state, Object.assign({}, action.course)];
