a53edae7850768035822a2371e2a79bd74f21cf6
quy <leuducquy@gmail.com>
2017-04-07 17:27:03 +0700

    modify courese list

--- src/components/course/CourseListRow.js
+++ src/components/course/CourseListRow.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import {Link} from 'react-router';
 
 const CourseListRow = ({course}) => {
+    console.log(JSON.stringify(course));
     return (
         <tr>
             <td><a href = {course.watchHref} target ="_blank" > Watch </a></td>
